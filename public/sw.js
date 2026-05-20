92eae2ebc4067a058136532b05b730b7538ad49d
mikeb-droid <mikeb@pestcofl.com>
2026-05-20 08:22:07 -0400

    Bust service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // DIALED Service Worker
-const CACHE='dialed-v1';
+const CACHE='dialed-v1779279727';
 const SHELL=['/','/index.html','/manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
